0b0f2a381cd86138ae3a5d9ad91b0f19fecde4f0
Basit ali <basitalyshah51214@gmail.com>
2024-05-01 04:16:38 -0700

    adding funtion to onbutton

--- lib/view/onboarding/widgets/on_button.dart
+++ lib/view/onboarding/widgets/on_button.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:sawari_pk/res/components/colors.dart';
 
 class OnButton extends StatelessWidget {
-  const OnButton({super.key});
+  final Function()? onTap;
+
+  const OnButton({super.key, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: AppColor.primaryColor,
-          border: Border.all(
-            width: 5,
-            color: const Color(0xffF0F0F0),
-          )),
-      child: const Icon(
-        Icons.arrow_forward_outlined,
-        color: AppColor.whiteColor,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColor.primaryColor,
+            border: Border.all(
+              width: 5,
+              color: const Color(0xffF0F0F0),
+            )),
+        child: const Icon(
+          Icons.arrow_forward_outlined,
+          color: AppColor.whiteColor,
+        ),
       ),
     );
   }
