87f8847e788cae55db390bc990838ffc26ce45b3
Basit ali <basitalyshah51214@gmail.com>
2024-05-01 04:08:16 -0700

    changes to add onboarding screen

--- lib/view/onboarding/onboarding_view1.dart
+++ lib/view/onboarding/onboarding_view1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sawari_pk/res/components/colors.dart';
+import 'package:sawari_pk/res/components/vertical_speacing.dart';
 
 class OnBoardingView1 extends StatelessWidget {
   const OnBoardingView1({super.key});
@@ -11,20 +12,118 @@ class OnBoardingView1 extends StatelessWidget {
       body: SafeArea(
           child: Column(
         children: [
-          Row(
-            children: [
-              Text(
-                "Hello!",
-                style: GoogleFonts.getFont(
-                  "Poppins",
-                  textStyle: const TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
-                    color: AppColor.titleColor,
+          const VerticalSpeacing(20),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Skip",
+                  style: GoogleFonts.getFont(
+                    "Urbanist",
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColor.textColor,
+                    ),
                   ),
                 ),
+              ],
+            ),
+          ),
+          const VerticalSpeacing(20),
+          Image.asset(
+            "images/onboarding1.png",
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20, top: 20),
+            child: Row(
+              children: [
+                Text(
+                  "Welcome to Sawari.",
+                  style: GoogleFonts.getFont(
+                    "Urbanist",
+                    textStyle: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      color: AppColor.titleColor,
+                    ),
+                  ),
+                ),
+                Text(
+                  "pk",
+                  style: GoogleFonts.getFont(
+                    "Urbanist",
+                    textStyle: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      color: AppColor.primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "Your Journey Starts Here Get ready to embark on a hassle-free travel experience with Sawari.pk. From booking to boarding, we've got all your travel needs covered.",
+              style: GoogleFonts.getFont(
+                "Urbanist",
+                textStyle: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: AppColor.textColor,
+                ),
               ),
-            ],
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 20.0,
+              right: 20,
+              bottom: 20,
+            ),
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 8,
+                      width: 19,
+                      decoration: BoxDecoration(
+                        color: AppColor.primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Container(
+                      height: 8,
+                      width: 8,
+                      decoration: BoxDecoration(
+                        color: AppColor.primaryColor.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    Container(
+                      height: 8,
+                      width: 8,
+                      decoration: BoxDecoration(
+                        color: AppColor.primaryColor.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           )
         ],
       )),
