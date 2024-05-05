bc45a6a301fe7d3c4c860045d1d5cfafda3fc1a2
Basit ali <basitalyshah51214@gmail.com>
2024-05-04 23:31:26 -0700

    changes to fix the confirm dialog

--- lib/view/home/booking_detail_view.dart
+++ lib/view/home/booking_detail_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sawari_pk/res/components/colors.dart';
@@ -5,11 +7,147 @@ import 'package:sawari_pk/res/components/icon_box.dart';
 import 'package:sawari_pk/res/components/profile_box.dart';
 import 'package:sawari_pk/res/components/rounded_button.dart';
 import 'package:sawari_pk/res/components/vertical_speacing.dart';
-import 'package:sawari_pk/view/home/confirm_booking.dart';
+import 'package:sawari_pk/utils/routes/routes_name.dart';
 import 'package:sawari_pk/view/home/widgets/booking_detail_widget.dart';
 
-class BookingDetailView extends StatelessWidget {
+class BookingDetailView extends StatefulWidget {
   const BookingDetailView({super.key});
+
+  @override
+  State<BookingDetailView> createState() => _BookingDetailViewState();
+}
+
+class _BookingDetailViewState extends State<BookingDetailView> {
+  void _showPopup(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Stack(
+          children: [
+            // fully blur with ImageFilter
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+            // Blurred background
+            ModalBarrier(
+              color: Colors.black.withOpacity(0.1),
+              dismissible: true,
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColor.whiteColor,
+                  borderRadius: BorderRadius.circular(
+                    10,
+                  ),
+                ),
+                width: 335,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const VerticalSpeacing(14),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Icon(Icons.close))
+                        ],
+                      ),
+                      const VerticalSpeacing(12),
+                      Text(
+                        "Are you sure to confirm\n the booking",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.getFont(
+                          "Urbanist",
+                          textStyle: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: AppColor.titleColor,
+                          ),
+                        ),
+                      ),
+                      const VerticalSpeacing(10),
+                      Text(
+                        "Once Your Seat is confirmed you can cancel it only within 24 hours .",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.getFont(
+                          "Urbanist",
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.textColor,
+                          ),
+                        ),
+                      ),
+                      const VerticalSpeacing(20),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Container(
+                                height: 46.0,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: AppColor.primaryColor,
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Cancels",
+                                    style: GoogleFonts.getFont(
+                                      "Urbanist",
+                                      textStyle: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                        color: AppColor.primaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                              child: RoundedButton(
+                                  title: "Confirm",
+                                  onpress: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      RoutesName.paymentMethod,
+                                    );
+                                  })),
+                        ],
+                      ),
+                      const VerticalSpeacing(60),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -210,12 +348,9 @@ class BookingDetailView extends StatelessWidget {
                           RoundedButton(
                               title: "Book Now",
                               onpress: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return const ConfirmDialog();
-                                  },
-                                );
+                                setState(() {
+                                  _showPopup(context);
+                                });
                               }),
                           const VerticalSpeacing(
                             40,
