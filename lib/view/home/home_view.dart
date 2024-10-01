62d2c0f56c928a17854b635159367ea3cfa0002a
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-10-01 19:21:50 +0500

    working on bus detail

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -26,6 +26,7 @@ class _HomeViewState extends State<HomeView> {
     homeViewViewModel.fetchVehicalList();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -178,7 +179,8 @@ class _HomeViewState extends State<HomeView> {
                                                 MaterialPageRoute(
                                                     builder: (context) =>
                                                         BusDetailView(
-                                                         popularSchedule: popularSchedules,
+                                                          popularSchedule:
+                                                              popularSchedules,
                                                         )));
                                           },
                                         ),
@@ -269,7 +271,9 @@ class _HomeViewState extends State<HomeView> {
                                                 MaterialPageRoute(
                                                     builder: (context) =>
                                                         BusDetailView(
-                                                           popularSchedule: popularSchedules,)));
+                                                          popularSchedule:
+                                                              popularSchedules,
+                                                        )));
                                           },
                                         ),
                                       ),
